de21c2b70acf1039657aa6c15e8ecf5416fc4d71
Peter-Buranchikov <piterburan@ya.ru>
2023-03-14 22:24:05 +0300

    Цикл приветствия пользователя

---
+++ Example/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();  
+
+if(username =="оля") 
+{
+Console.WriteLine("Ура, это же моя любимая Оля!");
+}
+else 
+{
+Console.Write("Привет, ");
+Console.WriteLine(username);
+}
